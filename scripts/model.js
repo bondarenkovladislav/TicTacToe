2d7e75ae8aa22191d3a45ba113d26b6fe828be1e
Vladislav Bondarenko <v.bondarenko1@uniyar.ac.ru>
2019-07-04 19:58:37 +0300

    Added Computer analysis of step predict

--- scripts/model.js
+++ scripts/model.js
@@ -1,4 +1,4 @@
-// generateMarker('11','x');
+
 // generateMarker({ix:1,iy:1},'x');
 // generateMarker({ix:2,iy:1},'x');
 // winner([{ix:1,iy:1},{ix:2,iy:2}, {ix:0,iy:0}]);
--- scripts/view.js
+++ scripts/view.js
@@ -2,9 +2,9 @@ let battlefield = document.querySelector('.battlefield');
 
 let idList = [];
 let objectList= null;
-let gameFinished = false;
 //Идикатор первого хода игрока
 let firstStep = true;
+let historyOpen = false;
 
 let userScore = 0;
 let computerScore= 0;
@@ -13,16 +13,19 @@ historyDisplay();
 battlefield.addEventListener('click',battlefieldListener);
 
 document.querySelector('#reset').addEventListener('click',x=>{
-    if(gameFinished) {
-        gameFinished = !gameFinished;
+        historyOpen=false;
+        resetColorHistory();
         clearScene();
         idList = [];
         battlefield.addEventListener('click',battlefieldListener);
         firstStep = !firstStep;
         if(!firstStep)
             computerStep();
-    }
 });
+// generateMarker('20','x');
+// generateMarker('02','x');
+// predictBestInOffDiagonal([{type:'X',id:'02'},{type:'X',id:'20'}]);
+// tryPredictBestStep();
 
 //Возвращает есть ли коллизия
 function checkCollision(checkingId){
@@ -71,14 +74,18 @@ let winner = function(markersIds) {
 };
 
 function computerStep() {
-    let genId;
+    if(tryPredictBestStep())
+        return;
 
+    let genId;
     do {
         genId = generateId();
     }
     while (checkCollision(genId));
-    generateMarker(genId,'x');
+    generateMarker(genId, 'x');
 }
+
+
 //Включая концы
 function randomInteger(min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1)
@@ -90,25 +97,26 @@ function generateId() {
     return `${randomInteger(0,2)}${randomInteger(0,2)}`;
 }
 
-function checkWinner() {
-    objectList = createListOfMarkers();
+function checkWinner(objectList) {
     // Если еще рано проверять
     if(objectList.length<5)
         return;
-    xList = objectList.filter(x=>{
+    let xList = objectList.filter(x=>{
         return x.type === 'X';
     });
-    oList = objectList.filter(x=>{
+    let oList = objectList.filter(x=>{
         return x.type === 'O';
     });
     if(checkEqualsCombinations(oList)) {
-        setScore(++userScore);
+        userScore = historyOpen?userScore:userScore+1;
+        setScore(userScore,computerScore);
         historyLog('o');
         endOfGame();
         return true;
     }
     if(checkEqualsCombinations(xList)){
-        setScore(++computerScore);
+        computerScore = historyOpen?computerScore:computerScore+1;
+        setScore(userScore,computerScore);
         historyLog('x');
         endOfGame();
         return true;
@@ -168,6 +176,7 @@ function checkEqualsRowsCols(list) {
     return null;
 }
 
+
 //Проверка на гравной диагонали
 function checkMainDiag(list) {
     let upper = list.filter(x=>{
@@ -201,6 +210,155 @@ function checkOffDiag(list) {
     return null;
 }
 
+function predictBestInRow(list){
+    let rowIndex = list[0].substring(0,1);
+    let predictIds= [`${rowIndex}0`,`${rowIndex}1`,`${rowIndex}2`];
+    list.forEach((x)=>{
+        let index = predictIds.findIndex(y=>{
+            return y ===x;
+        });
+        if(index!==-1)
+            predictIds.splice(index,1);
+    });
+    if(!checkCollision(predictIds[0])){
+        generateMarker(predictIds[0],'x');
+        return true;
+    }
+    return false;
+}
+
+function predictBestInColumn(list) {
+    let columnIndex = list[0].substring(1);
+    let predictIds= [`0${columnIndex}`,`1${columnIndex}`,`2${columnIndex}`];
+    list.forEach((x)=>{
+        let index = predictIds.findIndex(y=>{
+            return y ===x;
+        });
+        if(index!==-1)
+            predictIds.splice(index,1);
+    });
+    if(!checkCollision(predictIds[0])) {
+        generateMarker(predictIds[0], 'x');
+        return true;
+    }
+    return false;
+}
+
+function predictBestInMainDiagonal(list) {
+    let predictIds = [`00`,`11`,`22`];
+    list.forEach((x)=>{
+        let index = predictIds.findIndex(y=>{
+            return y ===x.id;
+        });
+        if(index!==-1)
+            predictIds.splice(index,1);
+    });
+    if(predictIds.length===1 && !checkCollision(predictIds[0])) {
+        generateMarker(predictIds[0], 'x');
+        return true;
+    }
+    return false;
+}
+
+function predictBestInOffDiagonal(list){
+    let predictIds = [`02`,`11`,`20`];
+    list.forEach((x)=>{
+        let index = predictIds.findIndex(y=>{
+            return y ===x.id;
+        });
+        if(index!==-1)
+            predictIds.splice(index,1);
+    });
+    if(predictIds.length===1 &&!checkCollision(predictIds[0])){
+        generateMarker(predictIds[0],'x');
+        return true;
+    }
+    return false;
+}
+
+function tryPredictBestStep() {
+    let markerList = createListOfMarkers();
+    let xList = markerList.filter(x=>{
+        return x.type === 'X';
+    });
+    let oList = markerList.filter(x=>{
+        return x.type === 'O';
+    });
+    let xRowPredictVariants = [];
+    let xCellPredictVariants = [];
+    let oRowPredictVariants = [];
+    let oCellPredictVariants = [];
+
+    for(let i =0;i<xList.length;i++){
+        let equalRowIds=[xList[i].id];
+        let equalColumnIds =[xList[i].id];
+        for(let j = 0;j<xList.length;j++){
+            if(i===j)
+                continue;
+            if(xList[i].id.substring(0,1) ===xList[j].id.substring(0,1)) equalRowIds.push(xList[j].id);
+            if(xList[i].id.substring(1) === xList[j].id.substring(1)) equalColumnIds.push(xList[j].id);
+        }
+        if(equalRowIds.length===2) xRowPredictVariants.push(equalRowIds);
+        if(equalColumnIds.length===2) xCellPredictVariants.push(equalColumnIds);
+    }
+
+    for(let i =0;i<oList.length;i++){
+        let equalRowIds=[oList[i].id];
+        let equalColumnIds =[oList[i].id];
+        for(let j = 0;j<oList.length;j++){
+            if(i===j)
+                continue;
+            if(oList[i].id.substring(0,1) ===oList[j].id.substring(0,1)) equalRowIds.push(oList[j].id);
+            if(oList[i].id.substring(1) === oList[j].id.substring(1)) equalColumnIds.push(oList[j].id);
+        }
+        if(equalRowIds.length===2) oRowPredictVariants.push(equalRowIds);
+        if(equalColumnIds.length===2) oCellPredictVariants.push(equalColumnIds);
+    }
+
+    for(let i=0;i<xRowPredictVariants.length;i++){
+        if(predictBestInRow(xRowPredictVariants[i]))
+            return true;
+    }
+    for(let i=0;i<xCellPredictVariants.length;i++){
+        if(predictBestInColumn(xCellPredictVariants[i]))
+            return true;
+    }
+
+    for(let i=0;i<oRowPredictVariants.length;i++){
+        if(predictBestInRow(oRowPredictVariants[i]))
+            return true;
+    }
+    for(let i=0;i<oCellPredictVariants.length;i++){
+        if(predictBestInColumn(oCellPredictVariants[i]))
+            return true;
+    }
+
+    let diagIds = xList.filter(x=>{
+       return x.id.substring(0,1) === x.id.substring(1);
+    });
+    if(predictBestInMainDiagonal(diagIds))
+        return true;
+    let offdiagIds = xList.filter(x=>{
+       return x.id ==='02' || x.id ==='20' || x.id === '11'
+    });
+    if(predictBestInOffDiagonal(offdiagIds))
+        return true;
+
+    diagIds = oList.filter(x=>{
+        return x.id.substring(0,1) === x.id.substring(1);
+    });
+    if(predictBestInMainDiagonal(diagIds))
+        return true;
+    offdiagIds = oList.filter(x=>{
+        return x.id ==='02' || x.id ==='20' || x.id === '11'
+    });
+    if(predictBestInOffDiagonal(offdiagIds))
+        return true;
+    return false;
+}
+
+
+
 function endOfGame() {
     battlefield.removeEventListener('click',battlefieldListener);
     gameFinished = true;
@@ -210,9 +368,9 @@ function battlefieldListener(e) {
     const id = `${rowIndex(e.target)}${cellIndex(e.target)}`;
     if(!checkCollision(id)) {
         generateMarker(id, 'o');
-        if(!checkWinner()){
+        if(!checkWinner(createListOfMarkers())){
             computerStep();
-            checkWinner();
+            checkWinner(createListOfMarkers());
         }
     }
 }
@@ -243,37 +401,37 @@ function setScore(){
 }
 
 function historyLog(winner) {
+    if(historyOpen)
+        return;
     let history = getHistory();
     if(!history)
         history = [];
     switch (winner){
         case 'x':{
-            history.push({
-                type:'X',
-                time:new Date().toLocaleTimeString('en-US'),
-
-            });
+            history.push(createHistoryElement('X'));
             localStorage.setItem('history',JSON.stringify(history));
             break;
         }
         case 'o':{
-            history.push({
-                type:'O',
-                time:new Date().toLocaleTimeString('en-US')
-            });
+            history.push(createHistoryElement('O'));
             localStorage.setItem('history',JSON.stringify(history));
             break;
         }
         case '-':{
-            history.push({
-                type:'Draw',
-                time:new Date().toLocaleTimeString('en-US')
-            });
+            history.push(createHistoryElement('Draw'));
             localStorage.setItem('history',JSON.stringify(history));
             break;
         }
     }
     historyDisplay();
+}
+
+function createHistoryElement(markerType) {
+    return{
+        type:markerType,
+        time:new Date().toLocaleTimeString('en-US'),
+        steps: createListOfMarkers()
+    }
 }
 
 function getHistory() {
@@ -297,6 +455,37 @@ function historyDisplay() {
     history.forEach(el=>{
         let hItem =document.createElement('div');
         hItem.textContent = `Winner: ${el.type}. Time: ${el.time}`;
+
+        hItem.addEventListener('click',x=>{
+            resetColorHistory();
+           repeatHystory(el);
+        });
+
+       hItem.onmouseover = function()
+        {
+            this.style.backgroundColor = "gray";
+        };
+       hItem.onmouseleave = function(){
+           this.style.backgroundColor = 'white';
+       };
+
+        hItem.className = 'hItem';
         histDisplayItem.prepend(hItem);
     });
+}
+
+function repeatHystory(histObject) {
+    historyOpen=true;
+    const steps = histObject.steps;
+    clearScene();
+    steps.forEach(x=>{
+       generateMarker(x.id,x.type.toLowerCase());
+    });
+    checkWinner(steps);
+}
+
+function resetColorHistory() {
+    document.querySelector('.history-display').childNodes.forEach(x=>{
+        x.style.backgroundColor = 'white';
+    })
 }
