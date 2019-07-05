ee98ee474c3610b1e26b6a9bd6b4cb0b50bae00c
Vladislav Bondarenko <v.bondarenko1@uniyar.ac.ru>
2019-07-05 19:25:28 +0300

    Commented EventListeners Module

--- scripts/eventListeners.js
+++ scripts/eventListeners.js
@@ -1,34 +1,61 @@
+/*
+    Модуль содержащий в себе все обработчики событий.
+ */
+
 import {clearScene, computerStep,checkCollision,generateMarker,checkWinner,createListOfMarkers} from "./functions.js";
 import {score,history} from "./initialise.js";
 import History from "./Classes/history.js"
 
+//Для смены очередности хода при начале игры используется переменная 'FirstStep'
+//'true', когда пользователь ходит первым
 let firstStep = true;
+
+//Для сокращения кода сохраним значение селектора
 let battlefield = document.querySelector('.battlefield');
 
+//Обработчик события сброса маркеров на поле при нажатии на кнопку 'ResetField' на сцене
 document.querySelector('#reset').addEventListener('click',resetField);
 
+//Обработчик при нажатии пользователя мышью внутри поля с клетками
 battlefield.addEventListener('click',battlefieldListener);
 
+//При нажатии на 'ClearHistory'
+//Очищается история
 document.querySelector('#clear-history').addEventListener('click',x=>{
     history.clearHistory();
 });
+//Сбрасывается счет матча
 document.querySelector('#clear-history').addEventListener('click',x=>{
     score.clearScore();
 });
+//Удаляются все маркеры на поле
 document.querySelector('#clear-history').addEventListener('click',resetField);
 
+/*
+    Удаляет маркеры на сцене, определяет очередность хода игроков
+ */
 
 function resetField() {
     history.setHistoryOpen(false);
     History.resetColorHistory();
     clearScene(battlefield);
+    //Необходимо восстановить обработчик клика на поле после вызова метода 'endOfGame' из 'functions.js',
+    //Отключающего его
     battlefield.addEventListener('click',battlefieldListener);
     firstStep = !firstStep;
     if(!firstStep)
         computerStep();
 }
 
+/*
+    При нажатии на клетку генерирует маркер игрока,
+    А следом и маркер компьютера.
+    Проверяет, не нажато ли на уже существующий маркер,
+    А также нет ли при текущем раскладе маркеров выигрышных комбинаций.
+ */
+
 export function battlefieldListener(e) {
+    //Получаем id элемента, на который был сделан клик
     const id = `${rowIndex(e.target)}${cellIndex(e.target)}`;
     if(!checkCollision(id)) {
         generateMarker(id, 'o');
@@ -39,9 +66,17 @@ export function battlefieldListener(e) {
     }
 }
 
+/*
+    Возвращает индекс строки на поле, в котором находится 'element'
+ */
+
 function rowIndex(element) {
     return element.closest('tr').rowIndex;
 }
+
+/*
+Возвращает индекс столбца на поле, в котором находится 'element'
+ */
 
 function cellIndex(element) {
     return element.closest('td').cellIndex
